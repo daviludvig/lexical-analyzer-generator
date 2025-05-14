42963e5ce0b697bd5567e956607b1eae6ec92d73
davi.ludvig <daviludvig@icloud.com>
2025-05-14 20:54:04 -0300

    Adiciona uniao de automatos por eps-trans

--- src/core/union.py
+++ src/core/union.py
@@ -15,15 +15,30 @@ def union(fa1: fa.FA, fa2: fa.FA) -> nfa.NFA:
     # Cria um novo autômato
     new_nfa = nfa.NFA(fa1.alphabet.union(fa2.alphabet))
 
+    # Copia os alvos da union
+    new_fa1 = fa1._cloneWithPrefix("fa1_")
+    new_fa2 = fa2._cloneWithPrefix("fa2_")
+
     # Cria um novo estado inicial
-    new_initial_state = fa.State(name="union_q0", is_final=False)
+    new_initial_state = fa.State(name="union_q0", is_final=False, is_initial=True)
     new_nfa.addState(new_initial_state)
 
+    # Desativa os estados iniciais dos autômatos copiados
+    new_fa1._disableInitialState()
+    new_fa2._disableInitialState()
+
     # Adiciona todos os estados e transições do primeiro autômato
-    new_nfa.addStates(fa1.states.copy())
-    new_nfa.addTransitions(fa1.transitions.copy())
+    new_nfa.addStates(new_fa1.states)
+    new_nfa.addTransitions(new_fa1.transitions)
 
     # Adiciona todos os estados e transições do segundo autômato
-    new_nfa.addStates(fa2.states.copy())
-    new_nfa.addTransitions(fa2.transitions.copy())
+    new_nfa.addStates(new_fa2.states)
+    new_nfa.addTransitions(new_fa2.transitions)
     
+    # Adiciona transições do novo estado inicial para os estados iniciais dos autômatos
+    t1 : fa.Transition = fa.Transition(source_state=new_initial_state, input_symbol="&", target_state=new_fa1.initial_state)
+    t2 : fa.Transition = fa.Transition(source_state=new_initial_state, input_symbol="&", target_state=new_fa2.initial_state)
+    
+    new_nfa.addTransitions({t1, t2})
+    
+    return new_nfa
